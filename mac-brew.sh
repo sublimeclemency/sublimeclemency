6a3c221ad2b5410acacb692ec6745ca72c6715df
sublimeclemency <sublimeclemency@gmail.com>
2024-01-06 17:57:23 +1100

    Create mac-brew.sh

---
+++ mac-brew.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+printf "hi, this bash script installs some brew packages, and is located at:\n$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )\nplease edit this to your preferences.\n"
+read -p "if you have already carefully edited this bash script, input y to run. " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+
+
+
+# common
+
+brew install wget
+
+brew install git
+
+brew install tree
+
+
+
+# cli operations on media files
+
+brew install ffmpeg
+
+# use ai models in cli
+# brew install aichat
+
+
+# autosuggestions
+brew install zsh-autosuggestions
+
+
+
+# python stuff
+
+brew install python3
+brew install poetry
+brew install pypy3
+
+
+
+# cli image operations and metadata reading
+brew install imagemagick
+
+
+# file sync
+brew install syncthing
+
+
+# hugo is a static site generator
+brew install hugo
+
+
+# all things related to node.js
+brew install nodejs
+# bun is a fast node package manager
+brew tap oven-sh/bun
+brew install bun
+
+
+
+# other
+## building c software
+brew install cmake
+## the go programming language
+brew install go
+## the rust programming language
+brew install rust
+
+
+# cli app for webp image format operations
+brew install webp
+
+
+# full-featured command-line application for reading and writing meta information in a wide variety of files
+brew install exiftool
