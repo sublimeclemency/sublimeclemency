c0fdf7ef2672b2ac5776a80ac2192502b5ecc002
sublimeclemency <sublimeclemency@gmail.com>
2024-06-16 05:24:25 +0000

    Update README.md

--- README.md
+++ README.md
@@ -1,11 +1,3 @@
 hi!
 
-awesome telegram bots and channels list at [telegram.txt](telegram.txt)
-
-awesome mac apps that can be installed with brew at [mac-apps-brew.txt](mac-apps-brew.txt)
-
-some useful brew packages are at [mac-brew.sh](mac-brew.sh)
-
 you can change many settings make your mac much more useful with [mac-settings.sh](mac-settings.sh)
-
-if you have any good tips or software to share, please create an issue!
--- pip.sh
+++
@@ -1,8 +0,0 @@
-printf "hi, this bash script installs some pip packages, and is located at:\n$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )\nplease edit this to your preferences.\n"
-read -p "if you have already carefully edited this bash script, and make sure you have python3 and pip3 installed. input y to run. " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
-
-# youtube downloader
-pip3 install yt-dlp
-
-# spotify downloader
-pip3 install spotify_dl
